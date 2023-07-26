e0c1a3fff3337e8b4c3ee74f29691157be6cfa8b
edo <39652158+edoderg@users.noreply.github.com>
2023-07-26 10:40:09 +0200

    edoStereo v0.0.3 launched
    +Added Crash Handler
    +Added Audio Channel Option
    +Support Server

--- edoStereo.plugin.js
+++ edoStereo.plugin.js
@@ -1,11 +1,11 @@
 /**
  * @name edoStereo
- * @version 0.0.2
+ * @version 0.0.3
  * @description [ENG] Adds stereo sound to Discord. [GER] Fügt Stereo Sound zu Discord hinzu.
- * @author ed.o
  * @authorLink https://github.com/edoderg
- * @website https://edoderg.github.io
+ * @website https://edoderg.github.io/
  * @source https://github.com/edoderg/edoStereo
+ * @invite MKT2XBbegu
  * @updateUrl https://github.com/edoderg/edoStereo/blob/main/edoStereo.plugin.js
  */
 module.exports = (() => {
@@ -15,15 +15,21 @@ module.exports = (() => {
     info: {
       name: "edoStereo",
       authors: [{ name: "ed.o", discord_id: "269831113919299584" }],
-      version: "0.0.2",
+      version: "0.0.3",
       description:
-        "[ENG] Adds stereo sound to Discord. Better Discord v1.9.3 | [GER] Fügt Stereo Sound zu Discord hinzu. ",
+        "[ENG] Adds stereo sound to Discord. | [GER] Fügt Stereo Sound zu Discord hinzu. Better Discord v1.9.3 ",
     },
     changelog: [
-      // changelog info
       {
         title: "Changelog",
         items: ["BetterDiscord Stereo Sound for 1.9.3"],
+      },
+      {
+        title: "New Features",
+        items: [
+          "Added a Error Handler when downloading ZeresPluginLibrary.",
+          "Added option to switch between audio channels (1.0 Mono Sound, 2.0 Normal Stereo Sound, 7.1 Surround Sound (Default) in settings.",
+        ],
       },
     ],
     defaultConfig: [
@@ -35,9 +41,20 @@ module.exports = (() => {
         note: "Warning for Discord Audio Features",
         value: true,
       },
+      {
+        type: "radio",
+        id: "stereoChannelOption",
+        name: "Stereo Channel Option",
+        note: "Select your preferred channel option:",
+        value: "7.1",
+        options: [
+          { name: "1.0 Mono Sound", value: "1.0" },
+          { name: "2.0 Normal Stereo Sound", value: "2.0" },
+          { name: "7.1 Surround Sound (Default)", value: "7.1" },
+        ],
+      },
     ],
   };
-
   return !global.ZeresPluginLibrary
     ? class {
         // placeholder for when zerespluginlibrary is missing
@@ -68,10 +85,19 @@ module.exports = (() => {
                 require("request").get(
                   "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
                   async (error, response, body) => {
-                    if (error)
-                      return require("electron").shell.openExternal(
-                        "https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+                    if (error) {
+                      console.error("Error downloading ZeresPluginLibrary:", error);
+                      BdApi.showConfirmationModal(
+                        "Download Error",
+                        "An error occurred while downloading ZeresPluginLibrary. Please try again later or download it manually from the official website.",
+                        {
+                          confirmText: "OK",
+                          cancelText: "Cancel",
+                        }
                       );
+                      return;
+                    }
+
                     await new Promise((r) =>
                       require("fs").writeFile(
                         require("path").join(
@@ -99,6 +125,10 @@ module.exports = (() => {
           return class edoStereo extends Plugin {
             // plugin start method
             onStart() {
+              BdApi.UI.showNotice(
+                "[edoStereo v.0.0.3] You can change your stereo channel in the plugin settings from now on! 😉",
+                { type: "info", timeout: 10000 }
+              );
               this.settingsWarning();
               const voiceModule = WebpackModules.getModule(
                 BdApi.Webpack.Filters.byPrototypeFields("updateVideoQuality")
@@ -112,12 +142,13 @@ module.exports = (() => {
                   if (thisObj) {
                     const setTransportOptions =
                       thisObj.conn.setTransportOptions;
+                    const channelOption = this.settings.stereoChannelOption;
                     thisObj.conn.setTransportOptions = function (obj) {
                       if (obj.audioEncoder) {
                         obj.audioEncoder.params = {
-                          stereo: "2",
+                          stereo: channelOption,
                         };
-                        obj.audioEncoder.channels = 2;
+                        obj.audioEncoder.channels = parseFloat(channelOption);
                       }
                       if (obj.fec) {
                         obj.fec = false;
@@ -159,7 +190,6 @@ module.exports = (() => {
             onStop() {
               Patcher.unpatchAll();
             }
-
             // creating settings panel
             getSettingsPanel() {
               const panel = this.buildSettingsPanel();
